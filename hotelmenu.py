ca7fb2160e191993d5876b047442b0590a64a82e
kamrulhasananik019 <kamrulhasananik019@gmail.com>
2024-09-23 06:38:38 +0600

    first commit

---
+++ hotelmenu.py
@@ -0,0 +1,15 @@
+menu = {
+    'pizza': 40,
+    'pasta': 50,
+    'burger': 30,
+    'salad': 20,
+    'fries': 15,
+    'soup': 25,
+    'steak': 60,
+    'sandwich': 35,
+    'tacos': 45,
+    'sushi': 70
+}
+
+
+print('welcome to our python Restaurant')
